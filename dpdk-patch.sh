0dc5e2f2ca049f8cc0bf2a05e4491397076beb15
rdoosty <doost@rice.edu>
2021-06-22 12:34:53 -0600

    automate dpdk patching

---
+++ dpdk-patch.sh
@@ -0,0 +1,4 @@
+sed -i '2340 s/DEFAULT/NONE/g' drivers/net/i40e/i40e_ethdev.c
+sed -i '2354 i\ \ \ \ \ \ \ \ i40e_aq_debug_write_global_register(hw, 0x0012A504, 0, NULL);' drivers/net/i40e/i40e_ethdev.c
+sed -i '693 s/DEFAULT/NONE/g' drivers/net/i40e/i40e_ethdev_vf.c
+sed -i '3871 i\\t\t\t\thw->mac.type != ixgbe_mac_82599EB &&' drivers/net/ixgbe/ixgbe_ethdev.c
--- install_oran_fhi.sh
+++ install_oran_fhi.sh
@@ -1,12 +1,15 @@
 # make sure to have enabled intel_iommu (intel_iommu=on) and set iommu to pt (iommu=pt) in grub
 wget https://fast.dpdk.org/rel/dpdk-19.11.8.tar.xz
 tar -xf dpdk-19.11.8.tar.xz
-#cd ~/dpdk-stable-19.11.8
+pushd .
+cd dpdk-stable-19.11.8
 # here apply patch from section A.2.3 https://docs.o-ran-sc.org/projects/o-ran-sc-o-du-phy/en/latest/Setup-Configuration_fh.html
-# ./usertools/dpdk-setup.sh
+source ../dpdk-patch.sh
+./usertools/dpdk-setup.sh
 # select 42 to select x86_64-native-linuxapp-icc and build
 # next step select 49 to install VFIO module
 # then 65 to exit
+popd
 echo 2 > /sys/class/net/enp37s0f0/device/sriov_numvfs
 echo 2 > /sys/class/net/enp37s0f1/device/sriov_numvfs
 echo 2 > /sys/bus/pci/devices/0000:25:00.0/sriov_numvfs
@@ -21,11 +24,7 @@ cd googletest-release-1.7.0
 cmake .
 make -j
 cd ..
-git clone https://github.com/o-ran-sc/o-du-phy.git
-cd o-du-phy
-git apply common.hpp.diff
-cd ..
-#source /opt/intel/oneapi/compiler/2021.1.1/env/vars.sh
+source /opt/intel/oneapi/compiler/2021.1.1/env/vars.sh
 export GTEST_ROOT=`pwd`/googletest-release-1.7.0
 export RTE_SDK=`pwd`/dpdk-stable-19.11.8
 export RTE_TARGET=x86_64-native-linuxapp-icc
