47f58454db650201414877052db0f0e593c399d6
westzhicanchen <zc38@rice.edu>
2021-06-24 11:36:01 -0500

    add -j to dpdk compile

--- dpdk-patch.sh
+++ dpdk-patch.sh
@@ -3,3 +3,4 @@ sed -i '2354 i\ \ \ \ \ \ \ \ i40e_aq_debug_write_global_register(hw, 0x0012A504
 sed -i '693 s/DEFAULT/NONE/g' drivers/net/i40e/i40e_ethdev_vf.c
 sed -i '3871 i\\t\t\t\thw->mac.type != ixgbe_mac_82599EB &&' drivers/net/ixgbe/ixgbe_ethdev.c
 sed -i '44 s/bin\/iccvars.sh/oneapi\/compiler\/2021.1.1\/env\/vars.sh/g' usertools/dpdk-setup.sh
+sed -i '79 s/install/install -j/g' usertools/dpdk-setup.sh
--- install_oran_fhi.sh
+++ install_oran_fhi.sh
@@ -10,14 +10,6 @@ source ../dpdk-patch.sh
 # in the next step select 49 to install VFIO module,
 # then 65 to exit
 popd
-echo 2 > /sys/class/net/enp37s0f0/device/sriov_numvfs
-echo 2 > /sys/class/net/enp37s0f1/device/sriov_numvfs
-echo 2 > /sys/bus/pci/devices/0000:25:00.0/sriov_numvfs
-sudo ip link set enp37s0f0 vf 1 mac 00:11:22:33:44:66 vlan 1
-sudo ip link set enp37s0f0 vf 0 mac 00:11:22:33:44:66 vlan 2
-echo 2 > /sys/bus/pci/devices/0000:25:00.1/sriov_numvfs
-sudo ip link set enp37s0f1 vf 1 mac 00:11:22:33:44:55 vlan 1
-sudo ip link set enp37s0f1 vf 0 mac 00:11:22:33:44:55 vlan 2
 wget https://github.com/google/googletest/archive/refs/tags/release-1.7.0.tar.gz
 tar -xf release-1.7.0.tar.gz
 cd googletest-release-1.7.0
@@ -32,6 +24,15 @@ export RTE_TARGET=x86_64-native-linuxapp-icc
 export XRAN_DIR="$CUR_DIR/fhi_lib"
 cd $XRAN_DIR
 source ./build.sh
+# Create VFs
+echo 2 > /sys/class/net/enp37s0f0/device/sriov_numvfs
+echo 2 > /sys/class/net/enp37s0f1/device/sriov_numvfs
+echo 2 > /sys/bus/pci/devices/0000:25:00.0/sriov_numvfs
+sudo ip link set enp37s0f0 vf 1 mac 00:11:22:33:44:66 vlan 1
+sudo ip link set enp37s0f0 vf 0 mac 00:11:22:33:44:66 vlan 2
+echo 2 > /sys/bus/pci/devices/0000:25:00.1/sriov_numvfs
+sudo ip link set enp37s0f1 vf 1 mac 00:11:22:33:44:55 vlan 1
+sudo ip link set enp37s0f1 vf 0 mac 00:11:22:33:44:55 vlan 2
 # Set hugepages for all the sockets on the system
 sudo sh -c "echo 16 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages"
 sudo sh -c "echo 16 > /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages"
